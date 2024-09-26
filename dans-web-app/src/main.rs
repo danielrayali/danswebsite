9caf3148e9f34600ec4715141aa404371703e3e0
Daniel Ali <daniel.ray.ali@gmail.com>
2024-09-25 22:12:03 -0400

    Add better resource finding/handling

--- dans-web-app/src/main.rs
+++ dans-web-app/src/main.rs
@@ -1,26 +1,32 @@
 #[macro_use]
 extern crate rocket;
 use rocket::fs::NamedFile;
-use rocket::http::ContentType;
-use rocket::response::stream::ReaderStream;
-use rocket::tokio::fs::File;
+use rocket::http::{ContentType, Status};
+use rocket::tokio::fs;
 use std::path::Path;
-
-#[get("/<name>")]
-async fn read_section(name: &str) -> std::io::Result<(ContentType, ReaderStream![File])> {
-    let file_to_serve = name.to_string() + ".html";
-    let file = File::open(file_to_serve).await?;
-    Ok((ContentType::HTML, ReaderStream::one(file)))
-}
 
 #[get("/")]
 async fn index() -> Option<NamedFile> {
     NamedFile::open(Path::new("blog.html")).await.ok()
 }
 
+#[get("/<name>")]
+async fn read_path(name: &str) -> (Status, Option<(ContentType, std::io::Result<String>)>) {
+    let file_path_str = name.to_string() + ".html";
+    let file_path = Path::new(&file_path_str);
+    if !file_path.exists() {
+        (Status::NotFound, None)
+    } else {
+        (
+            Status::Ok,
+            Some((ContentType::HTML, fs::read_to_string(file_path_str).await)),
+        )
+    }
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index])
-        .mount("/", routes![read_section])
+        .mount("/", routes![read_path])
 }
