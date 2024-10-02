0c5627d8e4f69aa05bda2c07cc3354d94376c3bb
Daniel Ali <daniel.ray.ali@gmail.com>
2024-10-01 20:46:30 -0400

    Use rocket's templating with tera
    - This will reduce redundancy provide greater feature/flexibility for additional content

--- dans-web-app/src/main.rs
+++ dans-web-app/src/main.rs
@@ -1,18 +1,46 @@
 #[macro_use]
 extern crate rocket;
-use rocket::fs::NamedFile;
 use rocket::http::{ContentType, Status};
 use rocket::tokio::fs;
+use rocket::Request;
+use rocket_dyn_templates::{context, Template};
 use std::path::Path;
 
+async fn get_blog() -> Template {
+    Template::render(
+        "blog",
+        context! {
+            dansstyle: fs::read_to_string("styles.css").await.unwrap()
+        },
+    )
+}
+
 #[get("/")]
-async fn index() -> NamedFile {
-    NamedFile::open(Path::new("blog.html")).await.unwrap()
+async fn index() -> Template {
+    get_blog().await
+}
+
+#[get("/blog")]
+async fn blog() -> Template {
+    get_blog().await
+}
+
+#[get("/about")]
+async fn about() -> Template {
+    Template::render(
+        "about",
+        context! {
+            dansstyle: fs::read_to_string("styles.css").await.unwrap()
+        },
+    )
 }
 
 #[get("/<name>")]
 async fn read_path(name: &str) -> (Status, Option<(ContentType, std::io::Result<String>)>) {
-    let file_path_str = name.to_string() + ".html";
+    let file_path_str = name.to_string();
+    if !file_path_str.ends_with(".html") && !file_path_str.ends_with(".css") {
+        return (Status::NotFound, None);
+    }
     let file_path = Path::new(&file_path_str);
     if !file_path.exists() {
         (Status::NotFound, None)
@@ -24,9 +52,15 @@ async fn read_path(name: &str) -> (Status, Option<(ContentType, std::io::Result<
     }
 }
 
+#[catch(404)]
+fn not_found(req: &Request) -> String {
+    format!("Oh no! We couldn't find the requested path '{}'", req.uri())
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index])
-        .mount("/", routes![read_path])
+        .register("/", catchers![not_found])
+        .attach(Template::fairing())
+        .mount("/", routes![index, blog, read_path, about])
 }
